25e342bd0cb8c18d429db4be2f3bc8c39d52b9c6
BigBrain3D <info@bigbrain3d.com>
2023-05-09 16:36:49 -0400

    Initial blank plugin

---
+++ Swapper3D_Package/static/js/Swapper3D.js
@@ -0,0 +1,29 @@
+/*
+ * View model for Swapper3D
+ *
+ * Author: BigBrain3D
+ * License: AGPLv3
+ */
+$(function() {
+    function Swapper3dViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: Swapper3dViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_Swapper3D, #tab_plugin_Swapper3D, ...
+        elements: [ /* ... */ ]
+    });
+});
